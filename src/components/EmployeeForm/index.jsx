1cb628cbe60db988d994f3f76ec74fdeeb852bd7
Giovani Aguirre <me@its-g.io>
2020-06-23 15:22:17 -0700

    Basic form skeleton and handle funciton added.

--- src/components/EmployeeForm/index.jsx
+++ src/components/EmployeeForm/index.jsx
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class index extends Component {
+  state = {
+    name: "",
+    department: "",
+    employee_annual_salary: "",
+    job_titles: "",
+  };
+
+  handleFormChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Employee Add Form</h1>
-      </div>
-    )
+      <form>
+        <input
+          onChange={this.handleFormChange}
+          name="name"
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          onChange={this.handleFormChange}
+          name="department"
+          type="text"
+          placeholder="Deparment"
+        />
+        <input
+          onChange={this.handleFormChange}
+          name="employee_annual_salary"
+          type="text"
+          placeholder="Annual Salary"
+        />
+        <input
+          onChange={this.handleFormChange}
+          name="job_titles"
+          type="text"
+          placeholder="Job Title"
+        />
+        <button>Submit</button>
+      </form>
+    );
   }
 }
